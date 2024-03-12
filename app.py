de399a146f690749940b3d2204f7ce4053d8e599
Felipe Rodrigues <felipeggrod@gmail.com>
2024-03-12 15:38:30 -0300

    back to 3.5, 4 is WAYYY TOO EXPENSIVE WTF

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vectorstore):
-    # llm = ChatOpenAI()
-    llm = ChatOpenAI(model="gpt-4")
+    llm = ChatOpenAI()
+    # llm = ChatOpenAI(model="gpt-4") #SUPER EXPENSIVE
 
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
 
